780d13634b0e9e1a34a759cbb3ad86c6bc64348d
Daniel <daniel.chin@nyu.edu>
2024-03-27 09:51:33 +0400

    utils

---
+++ music.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def pitch2freq(pitch: float) -> float:
+    return np.exp((pitch + 36.37631656229591) * 0.0577622650466621)
+
+def freq2pitch(f: float) -> float:
+    return np.log(f) * 17.312340490667562 - 36.37631656229591
--- shared.py
+++ shared.py
@@ -1,2 +1,6 @@
+import numpy as np
+
 import init as _
 from paths import *
+
+TWO_PI = np.pi * 2
