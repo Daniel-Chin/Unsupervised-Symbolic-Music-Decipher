0c19891124ea77e88b4157dc53e062be1395d033
Daniel <daniel.chin@nyu.edu>
2024-03-28 14:23:37 +0400

    fix dataset

--- tf_piano_dataset.py
+++ tf_piano_dataset.py
@@ -12,7 +12,9 @@ from shared import *
 
 class TransformerPianoDataset(Dataset):
     def __init__(
-        self, dir_path: str = TRANSFORMER_PIANO_DATASET_DIR, 
+        self, 
+        limit_n_notes: int, # in case positional encoding repeats
+        dir_path: str = TRANSFORMER_PIANO_DATASET_DIR, 
         truncate_to_size: Optional[int] = None, 
         device: torch.device = CPU, 
     ):
@@ -25,16 +27,21 @@ class TransformerPianoDataset(Dataset):
         self.Y = torch.zeros((
             len(self.stems), ENCODEC_N_BOOKS, N_TOKENS_PER_DATAPOINT, 
         ), dtype=torch.int16, device=device)
-        for stem_i, stem in enumerate(tqdm(self.stems, 'Load dataset')):
-            x: Tensor = torch.load(path.join(dir_path, f'{stem}_x.pt'))
-            y: Tensor = torch.load(path.join(dir_path, f'{stem}_y.pt'))
+        n_notes_array = torch.zeros(len(self.stems), dtype=torch.int)
+        for i, datapoint_id in enumerate(tqdm(self.stems, 'Load dataset')):
+            x: Tensor = torch.load(path.join(dir_path, f'{datapoint_id}_x.pt'))
+            y: Tensor = torch.load(path.join(dir_path, f'{datapoint_id}_y.pt'))
             n_notes, _ = x.shape
+            n_notes_array[i] = n_notes
             real_x = torch.zeros((n_notes, 1 + 1 + 88))
             real_x[:, :2] = x[:, :2]
             ladder = torch.arange(n_notes)
             real_x[ladder, 2 + x[ladder, 2].to(torch.int) - PIANO_RANGE[0]] = 1.0
             self.X.append(real_x.to(device))
-            self.Y[stem_i, :, :] = y
+            self.Y[i, :, :] = y
+        max_notes = n_notes_array.max().item()
+        print('At most', max_notes, 'notes in a datapoint.')
+        assert max_notes <= limit_n_notes
 
     def __len__(self):
         return len(self.stems)
