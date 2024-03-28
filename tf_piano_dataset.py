6fa3dcea0def3a679acec7a163d34fbc73971ef9
Daniel <daniel.chin@nyu.edu>
2024-03-28 20:45:46 +0400

    improve dataset

--- tf_piano_dataset.py
+++ tf_piano_dataset.py
@@ -15,19 +15,24 @@ class TransformerPianoDataset(Dataset):
         self, 
         limit_n_notes: int, # in case positional encoding repeats
         dir_path: str = TRANSFORMER_PIANO_DATASET_DIR, 
+        offset: int = 0, 
         truncate_to_size: Optional[int] = None, 
         device: torch.device = CPU, 
     ):
-        with open(path.join(dir_path, 'index.json'), encoding='utf-8') as f:
-            self.stems = json.load(f)
-        if truncate_to_size is not None:
-            assert truncate_to_size <= len(self.stems)
-            self.stems = self.stems[:truncate_to_size]
+        def getStems():
+            with open(path.join(dir_path, 'index.json'), encoding='utf-8') as f:
+                stems = json.load(f)
+            stems = stems[offset:]
+            if truncate_to_size is not None:
+                assert truncate_to_size <= len(stems)
+                stems = stems[:truncate_to_size]
+            return stems
+        self.stems = getStems()
         self.X = []
         self.Y = torch.zeros((
             len(self.stems), ENCODEC_N_BOOKS, N_TOKENS_PER_DATAPOINT, 
         ), dtype=torch.int16, device=device)
-        n_notes_array = torch.zeros(len(self.stems), dtype=torch.int)
+        n_notes_array = torch.zeros(len(self.stems))
         for i, datapoint_id in enumerate(tqdm(self.stems, 'Load dataset')):
             x: Tensor = torch.load(path.join(dir_path, f'{datapoint_id}_x.pt'))
             y: Tensor = torch.load(path.join(dir_path, f'{datapoint_id}_y.pt'))
@@ -39,9 +44,10 @@ class TransformerPianoDataset(Dataset):
             real_x[ladder, 2 + x[ladder, 2].to(torch.int) - PIANO_RANGE[0]] = 1.0
             self.X.append(real_x.to(device))
             self.Y[i, :, :] = y
-        max_notes = n_notes_array.max().item()
-        print('At most', max_notes, 'notes in a datapoint.')
+        max_notes = round(n_notes_array.max().item())
+        print('The densest piece has', max_notes, 'notes.')
         assert max_notes <= limit_n_notes
+        print('mean + 2std:', n_notes_array.mean().item() + 2 * n_notes_array.std().item())
 
     def __len__(self):
         return len(self.stems)
@@ -50,5 +56,5 @@ class TransformerPianoDataset(Dataset):
         return self.X[index], self.Y[index, :, :]
 
 if __name__ == '__main__':
-    dataset = TransformerPianoDataset()
+    dataset = TransformerPianoDataset(limit_n_notes=10000)
     import IPython; IPython.embed()
