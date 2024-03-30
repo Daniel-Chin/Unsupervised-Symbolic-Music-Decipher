f5a5d97f0afb86a73aaa0abbfffdfaa02c8d02fe
Daniel <daniel.chin@nyu.edu>
2024-03-30 12:03:34 +0400

    torchwork mini

--- shared.py
+++ shared.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import init as _
 from paths import *
+from torchwork_mini import *
 
 SEC_PER_DATAPOINT = 30
 ENCODEC_SR = 32000
@@ -12,16 +13,3 @@ ENCODEC_FPS = 50
 N_TOKENS_PER_DATAPOINT = SEC_PER_DATAPOINT * ENCODEC_FPS
 
 TWO_PI = np.pi * 2
-
-HAS_CUDA = torch.cuda.is_available()
-CUDA = torch.device("cuda:0")
-CPU  = torch.device("cpu")
-# print(f'{__name__ = }')
-if HAS_CUDA:
-    DEVICE = CUDA
-    print('We have CUDA.', flush=True)
-    gpu_name = torch.cuda.get_device_name(DEVICE)
-    print(f'{gpu_name = }', flush=True)
-else:
-    DEVICE = CPU
-    print("We DON'T have CUDA.", flush=True)
---
+++ torchwork_mini.py
@@ -0,0 +1,53 @@
+from typing import *
+import dataclasses
+from functools import lru_cache
+
+import torch
+import git
+import lightning as L
+
+__all__ = [
+    'HAS_CUDA', 'CUDA', 'CPU', 'DEVICE', 'getParams', 'getGradNorm', 
+    'getCommitHashAndAssertWorkingTreeClean', 'writeLightningHparams', 
+]
+
+HAS_CUDA = torch.cuda.is_available()
+CUDA = torch.device("cuda:0")
+CPU  = torch.device("cpu")
+# print(f'{__name__ = }')
+if HAS_CUDA:
+    DEVICE = CUDA
+    print('We have CUDA.', flush=True)
+    gpu_name = torch.cuda.get_device_name(DEVICE)
+    print(f'{gpu_name = }', flush=True)
+else:
+    DEVICE = CPU
+    print("We DON'T have CUDA.", flush=True)
+
+def getParams(optim: torch.optim.Optimizer):
+    s: List[torch.Tensor] = []
+    for param_group in optim.param_groups:
+        for param in param_group['params']:
+            param: torch.Tensor
+            if param.grad is not None:
+                s.append(param)
+    return s
+
+def getGradNorm(params: List[torch.Tensor]):
+    buffer = torch.zeros((len(params), ), device=DEVICE)
+    for i, param in enumerate(params):
+        grad = param.grad
+        assert grad is not None
+        buffer[i] = grad.norm(2)
+    return buffer.norm(2).cpu()
+
+@lru_cache(maxsize=1)
+def getCommitHashAndAssertWorkingTreeClean():
+    repo = git.Repo('.', search_parent_directories=True)
+    assert not repo.is_dirty()
+    return next(repo.iter_commits()).hexsha
+
+def writeLightningHparams(dataclassObject, litModule: L.LightningModule):
+    for k, v in dataclasses.asdict(dataclassObject).items():
+        litModule.save_hyperparameters(k, v)
+    litModule.save_hyperparameters('commit_hash', getCommitHashAndAssertWorkingTreeClean())
