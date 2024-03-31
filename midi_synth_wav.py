4b7bf4a1f6a615f55e9284507f37750916149580
Daniel <daniel.chin@nyu.edu>
2024-03-31 13:12:31 +0400

    debug

--- midi_synth_wav.py
+++ midi_synth_wav.py
@@ -1,3 +1,4 @@
+import os
 from subprocess import Popen
 from io import TextIOWrapper
 
@@ -6,23 +7,28 @@ from shared import *
 def midiSynthWav(
     midi_path: str, wav_path: str, 
     logStream: TextIOWrapper, do_fluidsynth_write_pcm: bool = False,
+    debug: bool = True, 
 ):
+    if debug:
+        os.system('which fluidsynth')
     pcm_path = wav_path + '.pcm'
     cmd = [
         'fluidsynth', '-ni', SOUNDFONT_FILE, midi_path,
         '-F', pcm_path if do_fluidsynth_write_pcm else wav_path, 
         '-r', str(ENCODEC_SR), 
     ]
-    print(cmd, file=logStream)
-    with Popen(cmd, stdout=logStream) as p:
+    print(cmd, file=logStream, flush=debug)
+    with Popen(cmd, stdout=logStream, stderr=logStream) as p:
         p.wait()
+    if debug:
+        logStream.flush()
     
     if do_fluidsynth_write_pcm:
-        print(file=logStream)
+        print(file=logStream, flush=debug)
         cmd = [
             'ffmpeg', '-f', 's16le', '-ar', str(ENCODEC_SR), 
             '-ac', '1', '-i', pcm_path, wav_path, 
         ]
-        print(cmd, file=logStream)
+        print(cmd, file=logStream, flush=debug)
         with Popen(cmd, stdout=logStream, stderr=logStream) as p:
             p.wait()
