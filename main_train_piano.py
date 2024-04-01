cc9a6ce6bb7a729a4a7e5b77749aa5b603700065
Daniel <daniel.chin@nyu.edu>
2024-04-02 01:03:14 +0400

    wide

--- main_train_piano.py
+++ main_train_piano.py
@@ -7,10 +7,10 @@ from tf_piano_lightning import train
 def main():
     initMainProcess()
     hParams = HParams(
-        d_model = 256,
+        d_model = 512,
         key_event_encoder_n_layers = 2,
         key_event_encoder_d_hidden = 1024,
-        tf_piano_n_head = 4,
+        tf_piano_n_head = 8,
         tf_piano_n_encoder_layers = 3,
         tf_piano_n_decoder_layers = 3,
         tf_piano_d_feedforward = 1024,
@@ -24,7 +24,7 @@ def main():
         max_epochs = 10,
         require_repo_working_tree_clean = True, 
     )
-    exp_name = currentTimeDirName() + '_piano'
+    exp_name = currentTimeDirName() + '_piano_wide'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     train(hParams, path.join(EXPERIMENTS_DIR, exp_name))
