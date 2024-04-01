bca455be88588b0a5dc3a4a7996cc795bf8413f3
Daniel <daniel.chin@nyu.edu>
2024-04-02 00:43:50 +0400

    hp

--- main_train_piano.py
+++ main_train_piano.py
@@ -8,8 +8,8 @@ def main():
     initMainProcess()
     hParams = HParams(
         d_model = 256,
-        key_event_encoder_n_layers = 1,
-        key_event_encoder_d_hidden = None,
+        key_event_encoder_n_layers = 2,
+        key_event_encoder_d_hidden = 1024,
         tf_piano_n_head = 4,
         tf_piano_n_encoder_layers = 3,
         tf_piano_n_decoder_layers = 3,
@@ -21,10 +21,10 @@ def main():
 
         lr = 1e-3,
         batch_size = 16,
-        max_epochs = 30,
+        max_epochs = 10,
         require_repo_working_tree_clean = True, 
     )
-    exp_name = currentTimeDirName() + '_piano_larger_set'
+    exp_name = currentTimeDirName() + '_piano'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     train(hParams, path.join(EXPERIMENTS_DIR, exp_name))
