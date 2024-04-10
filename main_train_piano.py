800fb24c185bfe23f96203ebaf91ff38e14b0b81
Daniel <daniel.chin@nyu.edu>
2024-04-11 01:23:28 +0400

    fix

--- main_train_piano.py
+++ main_train_piano.py
@@ -20,7 +20,7 @@ def main():
 
         cnn_piano_lr = 1e-3,
         cnn_piano_batch_size = 128,
-        cnn_piano_max_epochs = 100,
+        cnn_piano_max_epochs = 25,
         require_repo_working_tree_clean = True, 
     )
     exp_name = currentTimeDirName() + '_p_1024'
