c03fee270985d3d91c0c90baae46e7101ec6951f
Daniel <daniel.chin@nyu.edu>
2024-04-11 14:59:49 +0400

    hp

--- main_train_piano.py
+++ main_train_piano.py
@@ -24,10 +24,10 @@ def main():
         cnn_piano_lr = 1e-3,
         cnn_piano_lr_decay = 1.00, 
         cnn_piano_batch_size = 256,
-        cnn_piano_max_epochs = 800,
+        cnn_piano_max_epochs = 8000,
         require_repo_working_tree_clean = True, 
     )
-    exp_name = currentTimeDirName() + '_p_overfit_no_decay'
+    exp_name = currentTimeDirName() + '_p_overfit_long'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
