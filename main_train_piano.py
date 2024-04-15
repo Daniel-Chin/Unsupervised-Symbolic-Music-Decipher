8564532fb85fc2d6f336fc8e1cd7696392f0c83e
Daniel <daniel.chin@nyu.edu>
2024-04-15 10:04:37 +0400

    fc

--- main_train_piano.py
+++ main_train_piano.py
@@ -9,8 +9,10 @@ def main():
     initMainProcess()
     hParams = HParams(
         cnn_piano_architecture = [
-            (3, 256), 
-            (3, 512), 
+            (6, 512), 
+            (0, 512), 
+            (0, 512), 
+            (0, 512), 
         ], 
 
         cnn_piano_train_set_size = 8000, 
@@ -24,7 +26,7 @@ def main():
         cnn_piano_max_epochs = 1000,
         require_repo_working_tree_clean = True, 
     )
-    exp_name = currentTimeDirName() + '_p_small'
+    exp_name = currentTimeDirName() + '_p_fc'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
