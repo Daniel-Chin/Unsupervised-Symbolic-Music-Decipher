a0c9a5be506dbcd9fe8feb33b630be8ccfb410b5
Daniel <daniel.chin@nyu.edu>
2024-05-17 00:20:42 +0400

    exp

--- main_train_piano.py
+++ main_train_piano.py
@@ -55,24 +55,24 @@ def main():
 
         arch_type = PianoArchType.CNN_LSTM, 
         arch_hparam = CNN_LSTM_HParam(
-            entrance_n_channel = 1024, 
+            entrance_n_channel = 512, 
             blocks = [
                 [
-                    (1, 1024), 
-                    (1, 1024), 
+                    (1, 512), 
+                    (1, 512), 
                 ], 
                 [
-                    (1, 1024), 
-                    (1, 1024), 
+                    (1, 512), 
+                    (1, 512), 
                 ], 
             ], 
-            lstm_hidden_size = 1024,
+            lstm_hidden_size = 512,
             lstm_n_layers = 2,
             last_conv_kernel_radius = 3, 
-            last_conv_n_channel = 1024,
+            last_conv_n_channel = 512,
         ), 
 
-        dropout = 0.0, 
+        dropout = 0.2, 
 
         out_type = PianoOutType.EncodecTokens,
 
@@ -95,7 +95,7 @@ def main():
         require_repo_working_tree_clean = True, 
         # require_repo_working_tree_clean = False, 
     )
-    exp_name = currentTimeDirName() + '_p_wide'
+    exp_name = currentTimeDirName() + '_p_dropout'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
