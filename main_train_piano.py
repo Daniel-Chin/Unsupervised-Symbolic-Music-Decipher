efb1ca5aa73a840173d3a72527c9b1c79babf4c7
Daniel <daniel.chin@nyu.edu>
2024-05-20 14:21:42 +0400

    exp

--- main_train_piano.py
+++ main_train_piano.py
@@ -85,7 +85,7 @@ def main():
         do_validate = True,
 
         lr = 1e-3,
-        lr_decay = 0.998, 
+        lr_decay = 0.99, 
         batch_size = 64,
         # batch_size = 16,
         max_epochs = 180,
@@ -95,7 +95,7 @@ def main():
         require_repo_working_tree_clean = True, 
         # require_repo_working_tree_clean = False, 
     )
-    exp_name = currentTimeDirName() + '_p_decay_.998'
+    exp_name = currentTimeDirName() + '_p_decay_.99'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
