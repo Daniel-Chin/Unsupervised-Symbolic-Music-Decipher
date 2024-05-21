219661ba218724ef3aadb7f8acb5313ae0b298c5
Daniel <daniel.chin@nyu.edu>
2024-05-21 21:11:17 +0400

    exp

--- main_train_piano.py
+++ main_train_piano.py
@@ -88,7 +88,7 @@ def main():
         lr_decay = 0.99, 
         batch_size = 64,
         # batch_size = 16,
-        max_epochs = 180,
+        max_epochs = 200,
         # max_epochs = 3,
         overfit_first_batch = False, 
 
