d686d24b0bb095a74711c93ec183420be9e0bac7
Daniel <daniel.chin@nyu.edu>
2024-05-25 13:01:37 +0400

    add gpu

--- init.py
+++ init.py
@@ -12,5 +12,6 @@ load_dotenv(path.join(PROJ_DIR, 'active.env'))
 if GPU_NAME in (
     'NVIDIA GeForce RTX 3050 Ti Laptop GPU', 
     'NVIDIA GeForce RTX 3090', 
+    'NVIDIA A800 80GB PCIe',
 ):
     torch.set_float32_matmul_precision('high')
