d458662667fbf25be3468ecf7b68ca3d6147dbb3
Daniel <daniel.chin@nyu.edu>
2024-05-28 11:23:46 +0400

    exp

--- main_train_decipher.py
+++ main_train_decipher.py
@@ -13,14 +13,14 @@ def main():
         interpreter_sample_not_polyphonic = True,
 
         loss_weight_left = 1.0, 
-        loss_weight_right = 1.0, 
+        loss_weight_right = 0.0, 
 
         train_set_size = 8000, 
         val_set_size = 2000,
         # train_set_size = 16, 
         # val_set_size = 16,
 
-        lr = 1e-1, 
+        lr = 1e-3, 
         lr_decay = 1.0, 
         batch_size = 32, 
         # batch_size = 8, 
@@ -31,7 +31,7 @@ def main():
         require_repo_working_tree_clean = True, 
         # require_repo_working_tree_clean = False, 
     )
-    exp_name = currentTimeDirName() + '_d_lr-1'
+    exp_name = currentTimeDirName() + '_d_left_only'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
