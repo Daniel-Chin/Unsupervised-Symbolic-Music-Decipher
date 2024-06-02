abe84f77c577107db83daf69d2dec5aa8c0b8723
Daniel <daniel.chin@nyu.edu>
2024-06-02 20:04:12 +0400

    exp

--- main_train_piano.py
+++ main_train_piano.py
@@ -51,7 +51,7 @@ def main():
             blocks = [
             ], 
             lstm_hidden_size = 512,
-            lstm_n_layers = 2,
+            lstm_n_layers = 1,
             last_conv_kernel_radius = 3, 
             last_conv_n_channel = 512,
         ), 
@@ -82,7 +82,7 @@ def main():
         continue_from = None, 
         # WARNING: using `continue_from` has a bug: the validation set is newly split, so data leak.
     )
-    exp_name = currentTimeDirName() + '_p_tea_decay_0.99'
+    exp_name = currentTimeDirName() + '_p_tea_decay_0.99_l_1'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
