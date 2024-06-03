70ad80f0332243966499390a2e52f6ec81126249
Daniel <daniel.chin@nyu.edu>
2024-06-03 19:07:36 +0400

    exp

--- main_train_decipher.py
+++ main_train_decipher.py
@@ -8,13 +8,13 @@ from decipher_subjective_eval import decipherSubjectiveEval
 def main():
     initMainProcess()
     hParams = HParamsDecipher(
-        using_piano='2024_m06_d02@19_16_27_p_tea/version_0/checkpoints/epoch=49-step=62500.ckpt', 
+        using_piano='2024_m06_d03@14_52_28_p_tea/version_0/checkpoints/epoch=49-step=70350.ckpt', 
 
         interpreter_sample_not_polyphonic = False,
         init_oracle_w_offset = None, 
 
-        loss_weight_left = 0.0, 
-        loss_weight_right = 1.0, 
+        loss_weight_left = 1.0, 
+        loss_weight_right = 0.0, 
         loss_weight_anti_collapse = 1.0, 
 
         train_set_size = 8000, 
@@ -36,7 +36,7 @@ def main():
         require_repo_working_tree_clean = True, 
         # require_repo_working_tree_clean = False, 
     )
-    exp_name = currentTimeDirName() + '_d_poly_ac_r'
+    exp_name = currentTimeDirName() + '_d_poly_ac_l'
     if not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
