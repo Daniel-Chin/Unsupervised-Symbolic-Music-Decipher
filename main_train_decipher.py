0b41ee7de76954f937664d1b0054f79caa143738
Daniel <daniel.chin@nyu.edu>
2024-06-11 12:55:42 +0800

    exp

--- main_train_decipher.py
+++ main_train_decipher.py
@@ -14,7 +14,7 @@ def main():
             using_piano='2024_m06_d06@03_15_20_p_slow/version_0/checkpoints/epoch=149-step=211050.ckpt', 
             interpreter_sample_not_polyphonic = True,
             init_oracle_w_offset = None, 
-            loss_weight_anti_collapse = 0.0, 
+            loss_weight_anti_collapse = 1.0, 
         ), 
 
         # strategy = DecipherStrategy.Free,
@@ -62,7 +62,7 @@ def main():
     #     EXPERIMENTS_DIR, 
     #     "2024_m06_d06@17_07_04_d_m_sample_ac_1.0/version_0/checkpoints/epoch=29-step=15000.ckpt", 
     # )
-    exp_name = currentTimeDirName() + '_d_pi'
+    exp_name = currentTimeDirName() + '_d_chongqing'
     if hParams is not None and not hParams.require_repo_working_tree_clean:
         exp_name += '_dirty_working_tree'
     print(f'{exp_name = }', flush=True)
